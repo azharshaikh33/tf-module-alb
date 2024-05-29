e6314378f738ef175713a175dbfe60adc063d1ef
Azhar <azharshaikh33@gmail.com>
2024-05-29 10:37:30 +0400

    Auto Push

---
+++ security-group.tf
@@ -0,0 +1,55 @@
+# Create a security group for public alb
+resource "aws_security_group" "alb_public" {
+  name        = "roboshop-${var.ENV}-public-alb-sg"
+  description = "Allow public traffic"
+  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  # Inbound rules
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-alb-sg"
+  }
+}
+
+# Create a security group for private alb
+resource "aws_security_group" "alb_private" {
+  name        = "roboshop-${var.ENV}-private-alb-sg"
+  description = "Allow private traffic"
+  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  # Inbound rules
+  ingress {
+    description = "Allow HTTP traffic from internal traffic only"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+   }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-alb-sg"
+  }
+}
