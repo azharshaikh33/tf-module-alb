cf2f14af7b41d1cfb9099a9c2ad0ad05aa3a1d16
Azhar <azharshaikh33@gmail.com>
2024-06-09 19:01:38 +0400

    Auto Push

--- alb.tf
+++ alb.tf
@@ -4,8 +4,8 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL
   load_balancer_type = "application"
-  security_groups    = SG
-  subnets            = SUBNET_IDS
+  security_groups    = local.SG
+  subnets            = local.SUBNET_IDS
 
   tags = {
     name = var.ALB_NAME
